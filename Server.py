5e73fe1fefaf3c7a708c402adf1c451b81e295cd
Jason Chen <chenduochenduo@hotmail.com>
2015-05-19 05:14:26 -0400

    Server update

--- Server.py
+++ Server.py
@@ -224,21 +224,7 @@ def getTopLocation(locationList, topNum):
 
 if __name__ == "__main__":
 
-   # parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
-   # parser.add_option("--host", default='', type='string', action="store", dest="host", help="hostname (localhost)")
-   # parser.add_option("--port", default=8000, type='int', action="store", dest="port", help="port (8000)")
-   # parser.add_option("--ssl", default=0, type='int', action="store", dest="ssl", help="ssl (1: on, 0: off (default))")
-   # parser.add_option("--cert", default='./cert.pem', type='string', action="store", dest="cert", help="cert (./cert.pem)")
-   # parser.add_option("--ver", default=ssl.PROTOCOL_TLSv1, type=int, action="store", dest="ver", help="ssl version")
    
-   # (options, args) = parser.parse_args()
-
-   # cls = SimpleEcho
-
-   # if options.ssl == 1:
-   #    server = SimpleSSLWebSocketServer(options.host, options.port, cls, options.cert, options.cert, version=options.ver)
-   # else:	
-   #    server = SimpleWebSocketServer(options.host, options.port, cls)
 
    server = initServer()
 
